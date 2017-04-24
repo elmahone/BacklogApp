17b136371609ba9e599c97211b500b557579c812
Miika <miika.ahonen@metropolia.fi>
2017-04-24 11:29:30 +0300

    Changed API calls to smaller functions so they can be reused somewhere else, added check username validity route

--- app/routes.js
+++ app/routes.js
@@ -29,128 +29,61 @@ module.exports = (app, passport) => {
         }
     });
 
+    app.get('/validateUsername/:platform/:user', (req, res) => {
+        const platform = req.params.platform;
+        const username = req.params.user;
+        switch (platform) {
+            case 'xbox': {
+                getXboxUserId(username, (xuid, err) => {
+                    if (!err) {
+                        res.send('OK');
+                    } else {
+                        res.send(err);
+                    }
+                });
+                break;
+            }
+            case 'steam': {
+                checkSteamId(username, (steamid, err) => {
+                    if (!err) {
+                        res.send('OK');
+                    } else {
+                        res.send(err);
+                    }
+                });
+                break;
+            }
+        }
+
+
+    });
+
     app.get('/getGames/:platform/:user', (req, res) => {
         const platform = req.params.platform;
         const username = req.params.user;
-        let games = [];
-        let fails = [];
         switch (platform) {
             case 'xbox': {
-                let xboneReady = false;
-                let x360Ready = false;
-                // Get Xbox user id with given username
-                unirest.get('https://xboxapi.com/v2/xuid/' + username)
-                    .headers({'X-Auth': process.env.XBOX_API})
-                    .end(function (response) {
-                        console.log(response.body);
-                        let xuid = '';
-                        if (typeof response.body.error_code === 'undefined') {
-                            xuid = response.body;
-                            // Get users xbox one game library
-                            unirest.get('https://xboxapi.com/v2/' + xuid + '/xboxonegames')
-                                .headers({'X-Auth': process.env.XBOX_API})
-                                .end(function (response) {
-                                    const library = response.body.titles;
-                                    console.log(response.body.titles);
-                                    for (const game of library) {
-                                        if (game.maxGamerscore !== 0) {
-                                            games.push({
-                                                id: game.titleId.toString(16),
-                                                platform: platform,
-                                                name: game.name,
-                                                playTime: 0,
-                                                reviewScore: 0,
-                                            });
-                                        }
-                                    }
-                                    xboneReady = true;
-                                    if (xboneReady && x360Ready) {
-                                        res.send(games);
-                                    }
-                                });
-                            // Get users xbox 360 game library
-                            unirest.get('https://xboxapi.com/v2/' + xuid + '/xbox360games')
-                                .headers({'X-Auth': process.env.XBOX_API})
-                                .end(function (response) {
-                                    const library = response.body.titles;
-                                    console.log(response.body.titles);
-                                    for (const game of library) {
-                                        if (game.totalGamerscore !== 0) {
-                                            games.push({
-                                                id: game.titleId.toString(16),
-                                                platform: platform,
-                                                name: game.name,
-                                                playTime: 0,
-                                                reviewScore: 0,
-                                            });
-                                        }
-                                    }
-                                    x360Ready = true;
-                                    if (xboneReady && x360Ready) {
-                                        res.send(games);
-                                    }
-                                });
-                        } else {
-                            res.send('No gamertag ' + username + ' found');
-                        }
-                    });
+                getXboxUserId(username, (xuid, err) => {
+                    if (!err) {
+                        getXboxGames(xuid, (games) => {
+                            res.send(games);
+                        })
+                    } else {
+                        res.send(err);
+                    }
+                });
                 break;
             }
-
             case 'steam': {
-                // Get steam user id with given username
-                unirest.get(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.STEAM_API}&vanityurl=${username}`)
-                    .end(function (response) {
-                        const body = response.body.response;
-                        let steamid = '';
-                        // If username is not found assume user gave their steam id as username
-                        if (body.success === 42 || typeof body.steamid === 'undefined') {
-                            steamid = username
-                        } else {
-                            steamid = body.steamid;
-                        }
-                        // Get users steam library
-                        unirest.get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API}&steamid=${steamid}&format=json`)
-                            .end(function (response) {
-                                console.log(response.body);
-                                const library = response.body.response.games;
-                                let requests = library.length;
-                                let i = 0;
-                                for (const game of library) {
-                                    const appid = game.appid;
-                                    i++;
-                                    // Small delay so api doesn't get overloaded
-                                    setTimeout(() => {
-                                        // Get game name for each game in library
-                                        unirest.get(`https://store.steampowered.com/api/appdetails?appids=${appid}`)
-                                            .end(function (response) {
-                                                requests--;
-                                                console.log(requests);
-                                                console.log(appid);
-                                                if (response.body !== null && response.body[appid].success) {
-                                                    games.push(
-                                                        {
-                                                            id: appid,
-                                                            platform: platform,
-                                                            name: response.body[appid].data.name,
-                                                            playTime: 0,
-                                                            reviewScore: 0,
-                                                        });
-                                                    console.log('Games in Lib: ' + games.length);
-                                                }
-                                                else {
-                                                    fails.push(appid);
-                                                }
-                                                if (requests === 0) {
-                                                    //DONE
-                                                    res.send(games);
-                                                }
-
-                                            });
-                                    }, i * 250);
-                                }
-                            });
-                    });
+                checkSteamId(username, (library, err) => {
+                    if (!err) {
+                        getSteamGames(library, (games) => {
+                            res.send(games);
+                        });
+                    } else {
+                        res.send(err);
+                    }
+                });
                 break;
             }
             default: {
@@ -187,4 +120,149 @@ module.exports = (app, passport) => {
             res.send(json);
         });
     });
+
+
+    // FUNCTIONS =============================
+
+    // check steam id validity. if valid return library
+    const checkSteamId = (username, cb = null) => {
+        username = encodeURIComponent(username.trim());
+        // Get steam user id with given username
+        unirest.get(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.STEAM_API}&vanityurl=${username}`)
+            .end(function (response) {
+                const body = response.body.response;
+                let steamid = '';
+                // If username is not found assume user gave their steam id as username
+                if (body.success === 42 || typeof body.steamid === 'undefined') {
+                    steamid = username
+                } else {
+                    steamid = body.steamid;
+                }
+                // Get users steam library
+                unirest.get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API}&steamid=${steamid}&format=json`)
+                    .end(function (response) {
+                        if (typeof response.body.response !== 'undefined') {
+                            const library = response.body.response.games;
+                            cb(library, null);
+                        } else {
+                            cb(null, 'Invalid SteamID or Steam User');
+                        }
+                    })
+
+            });
+    };
+
+    // Loop through users library and get each game's name
+    const getSteamGames = (library, cb) => {
+        let requests = library.length;
+        let games = [];
+        let i = 0;
+        for (const game of library) {
+            i++;
+            // Small delay so api doesn't get overloaded
+            setTimeout(() => {
+                requests--;
+                getSteamGameInformation(game.appid, (game) => {
+                    games.push(game);
+                });
+                if (requests === 0) {
+                    //DONE
+                    cb(games);
+                }
+            }, i * 220);
+        }
+    };
+
+    // Get single games information
+    const getSteamGameInformation = (appid, cb) => {
+        const platform = 'steam';
+        // Get game name for each game in library
+        unirest.get(`https://store.steampowered.com/api/appdetails?appids=${appid}`)
+            .end(function (response) {
+                if (response.body !== null && response.body[appid].success) {
+                    const game = {
+                        id: appid,
+                        platform: platform,
+                        name: response.body[appid].data.name,
+                        playTime: 0,
+                        reviewScore: 0,
+                    };
+                    cb(game);
+                }
+            });
+    };
+
+    // Get xbox users id with username
+    const getXboxUserId = (username, cb = null) => {
+        username = encodeURIComponent(username.trim());
+        unirest.get('https://xboxapi.com/v2/xuid/' + username)
+            .headers({'X-Auth': process.env.XBOX_API})
+            .end(function (response) {
+                console.log(response.body);
+                if (response.body.error_code) {
+                    if (cb) {
+                        cb(null, 'Invalid gamertag');
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if (cb) {
+                        cb(response.body, null);
+                    } else {
+                        return response.body;
+                    }
+                }
+            });
+    };
+
+    // Get xbox users games library with id
+    const getXboxGames = (xuid, cb) => {
+        const platform = 'xbox';
+        let xboneReady = false;
+        let x360Ready = false;
+        let games = [];
+        // Get Xbox user id with given username
+        // Get users xbox one game library
+        unirest.get('https://xboxapi.com/v2/' + xuid + '/xboxonegames')
+            .headers({'X-Auth': process.env.XBOX_API})
+            .end(function (response) {
+                const library = response.body.titles;
+                for (const game of library) {
+                    if (game.maxGamerscore !== 0) {
+                        games.push({
+                            id: game.titleId.toString(16), // Convert to game id hash
+                            platform: platform,
+                            name: game.name,
+                            playTime: 0,
+                            reviewScore: 0,
+                        });
+                    }
+                }
+                xboneReady = true;
+                if (xboneReady && x360Ready) {
+                    cb(games);
+                }
+            });
+        // Get users xbox 360 game library
+        unirest.get('https://xboxapi.com/v2/' + xuid + '/xbox360games')
+            .headers({'X-Auth': process.env.XBOX_API})
+            .end(function (response) {
+                const library = response.body.titles;
+                for (const game of library) {
+                    if (game.totalGamerscore !== 0) {
+                        games.push({
+                            id: game.titleId.toString(16), // Convert to game id hash
+                            platform: platform,
+                            name: game.name,
+                            playTime: 0,
+                            reviewScore: 0,
+                        });
+                    }
+                }
+                x360Ready = true;
+                if (xboneReady && x360Ready) {
+                    cb(games);
+                }
+            });
+    }
 };
